b4b992164928ec3e540e8883e83b936fffb0147f
Brandon John-Freso <brandonjf@gmail.com>
2023-04-12 03:47:26 -0400

    implement Circular password and cleanup

--- include/CircularQueue.h
+++ include/CircularQueue.h
@@ -1,48 +1,93 @@
-#include <iostream>
 #include <vector>
 
-template <typename T> class CircularQueue {
+template<typename T>
+class CircularQueue {
 public:
-    CircularQueue(int capacity) : capacity_(capacity), size_(0), head_(0), tail_(0) {
-        buffer_.resize(capacity_);
+    CircularQueue(size_t size) {
+        m_size = size;
+        m_data = new T[size];
+        m_head = 0;
+        m_tail = 0;
+        m_count = 0;
+    }
+
+    ~CircularQueue() {
+        delete[] m_data;
     }
 
     void enqueue(const T& value) {
-        if (size_ == capacity_) {
-            dequeue();
+        if (m_count == m_size) {
+            m_data[m_head] = value;
+            m_head = (m_head + 1) % m_size;
+            m_tail = (m_tail + 1) % m_size;
+        } else {
+            m_data[m_tail] = value;
+            m_tail = (m_tail + 1) % m_size;
+            m_count++;
         }
-        buffer_[tail_] = value;
-        tail_ = (tail_ + 1) % capacity_;
-        size_++;
     }
 
     T dequeue() {
-        if (size_ == 0) {
-            std::cerr << "Error: Queue underflow" << std::endl;
-            return T();
+        if (m_count == 0) {
+            throw std::out_of_range("queue is empty");
         }
-        T value = buffer_[head_];
-        head_ = (head_ + 1) % capacity_;
-        size_--;
+        T value = m_data[m_head];
+        m_head = (m_head + 1) % m_size;
+        m_count--;
         return value;
     }
 
-    T peek(int index) const {
-        if (index >= size_) {
-            std::cerr << "Error: Invalid index" << std::endl;
-            return T();
+    T peek() const {
+        if (m_count == 0) {
+            throw std::out_of_range("queue is empty");
         }
-        return buffer_[(head_ + index) % capacity_];
+        return m_data[m_head];
     }
 
-    int size() const {
-        return size_;
+    T peekAt(size_t index) const {
+        if (index >= m_count) {
+            throw std::out_of_range("index out of range");
+        }
+        return m_data[(m_head + index) % m_size];
+    }
+
+    bool isEmpty() const {
+        return m_count == 0;
+    }
+
+    bool isFull() const {
+        return m_count == m_size;
+    }
+
+    size_t size() const {
+        return m_count;
+    }
+
+    std::vector<T> toList() const {
+        std::vector<T> vec;
+        vec.reserve(m_count);
+        for (size_t i = 0; i < m_count; i++) {
+            vec.push_back(m_data[(m_head + i) % m_size]);
+        }
+        return vec;
+    }
+
+    bool operator==(const std::vector<T>& vec) const {
+        if (m_count != vec.size()) {
+            return false;
+        }
+        for (size_t i = 0; i < m_count; i++) {
+            if (m_data[(m_head + i) % m_size] != vec[i]) {
+                return false;
+            }
+        }
+        return true;
     }
 
 private:
-    std::vector<T> buffer_;
-    int capacity_;
-    int size_;
-    int head_;
-    int tail_;
+    T* m_data;
+    size_t m_size;
+    size_t m_head;
+    size_t m_tail;
+    size_t m_count;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,16 @@ int sensorValue = 0; // variable to store the value coming from the sensor
 int loopsPressed = 0;
 
 /** Password Related Vars*/
-enum clickType {s, d,};
+enum clickType
+{
+  s,
+  d,
+};
 clickType lastEntered;
 int passwordLength = 4;
-int myPassword[4] = {s, d, d, s};
-CircularQueue<int> pwQueue(4);
+std::vector<clickType> pwVect = {s, d, s, d};
+
+CircularQueue<clickType> pwQueue(4);
 int currentIndex;
 bool lastEnteredCorrect = false;
 
@@ -26,13 +31,7 @@ bool needsRelayReset = false;
 String currPassInfo = "Pass";
 String currIndexInfo = "Info";
 String currVerificationInfo = "Verification";
-// StackArray <clickType> stack;
-
-// threshold is 400 for the actual door / 320 for external dc power at 5v
-// 1800 for the heltex
-// OneButton button(sensorPin, true, false, true, 1900);
 OneButton button(sensorPin, true);
-CircularQueue<int> queue(5);
 
 void triggerRelay()
 {
@@ -41,36 +40,31 @@ void triggerRelay()
   digitalWrite(relayPin, LOW);
 }
 
-void outputPassInfo(String s){
- currPassInfo = s;
+void outputPassInfo(String s)
+{
+  currPassInfo = s;
 }
 
-void outputIndexInfo(String s){
- currIndexInfo = s;
+void outputIndexInfo(String s)
+{
+  currIndexInfo = s;
 }
-void outputVerificationInfo(String s){
- currVerificationInfo = s;
+void outputVerificationInfo(String s)
+{
+  currVerificationInfo = s;
 }
 
 void verifyLastEntered()
 {
   Serial.println(lastEntered);
 
-  if (lastEntered == myPassword[currentIndex])
+  pwQueue.enqueue(lastEntered);
+
+  if (pwQueue == pwVect)
   {
-    currentIndex++;
-    Serial.println("good");
-    outputPassInfo("good");
+    Serial.println("Password Accepted!");
+    // triggerRelay(); -- commented until we can get the relays working at 3v
   }
-  else
-  {
-    currentIndex = 0;
-    Serial.println("bad");
-    outputPassInfo("bad");
-  }
-  Serial.println("current index set to:");
-  outputIndexInfo("Index now:" + String(currentIndex));
-  Serial.println(currentIndex);
 }
 
 void doubleClick()
@@ -80,25 +74,6 @@ void doubleClick()
   verifyLastEntered();
 } // doubleClick
 
-void verifyPassword()
-{
-  Serial.println("current size of pw set to:");
-  if (currentIndex == passwordLength)
-  {
-    Serial.println("Verified");
-    outputVerificationInfo("Verified");
-    
-    //triggerRelay(); -- commented until we can get the relays working at 3v
-  }
-  else
-  {
-    Serial.println("Denied");
-    outputVerificationInfo("Denied");
-  }
-  currentIndex = 0;
-  outputIndexInfo("Index now:" + String(currentIndex));
-}
-
 void click()
 {
   Serial.println("CLICKED");
@@ -106,66 +81,40 @@ void click()
   verifyLastEntered();
 } // click
 
-void longPress()
-{
-  Serial.println("LONG PRESSED");
-  verifyPassword();
-} // click
-
 void setup()
 {
   Serial.begin(115200);
-  while (!Serial);
+  while (!Serial)
+    ;
   pinMode(LED_BUILTIN, OUTPUT);
-  // pinMode(sensorPin, INPUT_PULLUP);  
   pinMode(sensorPin, INPUT_PULLUP);
 
   Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Disable*/, true /*Serial Enable*/);
-  Heltec.display->setFont(ArialMT_Plain_10); 
-  Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT); 
+  Heltec.display->setFont(ArialMT_Plain_10);
+  Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
 
   Serial.println("Monitor connected");
   Serial.println("Starting");
-  // declare the ledPin as an OUTPUT:
-  // pinMode(ledPin, OUTPUT);
   pinMode(relayPin, OUTPUT);
   button.attachDoubleClick(doubleClick);
   button.attachClick(click);
-  button.attachLongPressStop(longPress);
+  // button.attachLongPressStop(longPress);
 }
 
 void loop()
 {
   Heltec.display->clear();
-  /*
-  int sensorState = digitalRead(sensorPin);
-  int buttonPressed = sensorState == LOW;
-  int prevLoopsPressed = loopsPressed;
-  if (buttonPressed){
-    loopsPressed++;
-    // Serial.print("Button Pressed Cnt: ");
-    // Serial.println(loopsPressed);
-  }
-  Serial.println(sensorState);
-  */
-  
-  
-  // read the value from the sensor:
-
   button.tick();
 
   // sensorValue = analogRead(sensorPin);
-  // int digval = digitalRead(sensorPin);  
+  // int digval = digitalRead(sensorPin);
   // String sensValStr = String(sensorValue);
   // String sensValStr = String(digval);
   // Heltec.display->drawString(0,0, sensValStr);
-  
   // Heltec.display->drawString(0,0, String(loopsPressed));
-  Heltec.display->drawString(0,0, button.getState());
-  Heltec.display->drawString(0,10, currPassInfo);
- Heltec.display->drawString(0,20,currIndexInfo);
- Heltec.display->drawString(0,30, currVerificationInfo);
+  Heltec.display->drawString(0, 0, button.getState());
+  Heltec.display->drawString(0, 10, currPassInfo);
+  Heltec.display->drawString(0, 20, currIndexInfo);
+  Heltec.display->drawString(0, 30, currVerificationInfo);
   Heltec.display->display();
-  // Serial.println(digval);
-// Serial.println(sensorValue);
 }
